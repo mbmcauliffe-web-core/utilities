8ba6f3e7f0994f48e03dcef6b63db469e4b1a2e7
MBMcAuliffe <michael@mbmcauliffe.com>
2022-12-09 10:51:32 -0600

    Different MailJet API Configuration

--- src/mailjet.js
+++ src/mailjet.js
@@ -1,33 +1,39 @@
-function sendMail(apiKey, secretKey, senderAddress, senderName, recipientAddress, subject, htmlBody){
+function sendMail (apiKey, secretKey, senderAddress, senderName, recipientAddress, subject, htmlBody) {
 
-  const mailjet = require ('node-mailjet')
-  .connect(apiKey, secretKey)
+  const Mailjet = require('node-mailjet');
+  const mailjet = Mailjet.apiConnect(
+      apiKey,
+      secretKey,
+  );
+
   const request = mailjet
-  .post("send", {'version': 'v3.1'})
-  .request({
-    "Messages":[
-      {
-        "From": {
-          "Email": senderAddress,
-          "Name": senderName
-        },
-        "To": [
-          {
-            "Email": recipientAddress
-          }
-        ],
-        "Subject": subject,
-        "HTMLPart": htmlBody
-      }
-    ]
-  })
+          .post('send', { version: 'v3.1' })
+          .request({
+            Messages: [
+              {
+                From: {
+                  Email: senderAddress,
+                  Name: senderName
+                },
+                To: [
+                  {
+                    Email: recipientAddress//,
+                    //Name: "passenger 1"
+                  }
+                ],
+                Subject: subject,
+                HTMLPart: htmlBody
+              }
+            ]
+          })
+
   request
-    .then((result) => {
-      console.log(result.body)
-    })
-    .catch((err) => {
-      console.log(err.statusCode)
-    })
+      .then((result) => {
+          console.log(result.body)
+      })
+      .catch((err) => {
+          console.log(err.statusCode)
+      })
 
 }
 
