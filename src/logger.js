043be15d5aae303470297a29f4082acf3619be2e
Michael B McAuliffe <michael@mbmcauliffe.com>
2023-02-01 09:46:57 -0600

    Added time-response to logger

--- src/logger.js
+++ src/logger.js
@@ -1,50 +1,57 @@
 const fs = require("fs");
+let logPath;
+let logStream;
 
-// Create a Logs Directory if one does not already exist
-if (!fs.existsSync("./logs")){
-    fs.mkdirSync("./logs");
-}
+function init () {
 
-// Create a Write Stream to a new CSV file for Requests
-const logPath = "./logs/" + "Request-Log_" + new Date().toISOString().replace(/-/g, ".").replace(/:/g, ".") + ".csv";
-const logStream = fs.createWriteStream(logPath, {flags:'a'});
-
-async function logRequest(req, res, next=()=>{}, time=""){
-
-	//Standardize the length of the time String
-	if ( time !== "" ) {
-		time = String(time).padStart(5, '0');
+	if (!fs.existsSync("./logs")){
+	    fs.mkdirSync("./logs");
 	}
 
-	// Replace the value of Body Objects with the Key "password" prior to logging 
+	logPath = "./logs/" + "Request-Log_" + new Date().toISOString().replace(/-/g, ".").replace(/:/g, ".") + ".csv";
+	logStream = fs.createWriteStream(logPath, {flags:'a'});
+
+}
+
+function removePasswords ( reqBody ) {
+
 	bodyString = '';
-	for(i in req.body){
+
+	for( i in reqBody ) {
+
 		if(i == "password"){
+
 			bodyString = bodyString + i + ": " + "**********" + ", ";
+
 		} else{
-			bodyString = bodyString + i + ": " + req.body[i] + ", ";
+
+			bodyString = bodyString + i + ": " + reqBody[i] + ", ";
+
 		}
 		
 	}
 
-	// Gather the Log Data
+	return bodyString
+
+}
+
+function logRequest (req, res, time=""){
+
 	const log = {
 		url: req.url,
 		method: req.method,
-		body: bodyString,
-		ip: req.connection.remoteAddress
+		body: removePasswords( req.body ),
+		ip: req.connection.remoteAddress,
+		time: "ResTime: " + Math.floor( time * 1000 ) / 1000 + "ms"
 	}
 
-	// Print Log Data to the Console
-	const consoleOutput = log.ip + " | ToS (ms)" + time + " | " + log.method + " | " + log.url + " | " + log.body;
+	const consoleOutput = log.ip + " | " + log.time + " | " + log.method + " | " + log.url + " | " + log.body;
 	console.log(consoleOutput);
 
-	// Add a timestamp and write Log Data to the Log Stream
-	const logOutput = new Date().toISOString() + "," + log.ip + "," + log.method + "," + log.url + "," + log.body.split(",").join(" | ") + "\n";
+	const logOutput = new Date().toISOString() + "," + log.ip + "," + log.time + "," + log.method + "," + log.url + "," + log.body.split(",").join(" | ") + "\n";
 	logStream.write(logOutput);
-
-	next();
 	
 }
 
+init();
 module.exports = logRequest;
