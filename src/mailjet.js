345cff3472b2ef165d6b19649db5f11b100bf9f8
Michael B McAuliffe <michael@mbmcauliffe.com>
2023-07-22 09:25:06 -0500

    Added CC support to mailjet

--- src/mailjet.js
+++ src/mailjet.js
@@ -1,4 +1,4 @@
-function sendMail ( apiKey, secretKey, senderAddress, senderName, recipientAddress, subject, htmlBody, attachments=[] ) {
+function sendMail ( apiKey, secretKey, senderAddress, senderName, recipientAddress, subject, htmlBody, attachments=[], carbonCopy ) {
 
   const Mailjet = require('node-mailjet');
   const mailjet = Mailjet.apiConnect(
@@ -6,27 +6,35 @@ function sendMail ( apiKey, secretKey, senderAddress, senderName, recipientAddre
       secretKey,
   );
 
+  let messageObject = {
+    From: {
+      Email: senderAddress,
+      Name: senderName
+    },
+    To: [
+      {
+        Email: recipientAddress//,
+        //Name: "passenger 1"
+      }
+    ],
+    Subject: subject,
+    HTMLPart: htmlBody,
+    Attachments: attachments
+  };
+
+  if ( carbonCopy ) {
+
+    messageObject.Cc:{
+      Email: carbonCopy
+    }
+
+  }
+
   const request = mailjet
-          .post('send', { version: 'v3.1' })
-          .request({
-            Messages: [
-              {
-                From: {
-                  Email: senderAddress,
-                  Name: senderName
-                },
-                To: [
-                  {
-                    Email: recipientAddress//,
-                    //Name: "passenger 1"
-                  }
-                ],
-                Subject: subject,
-                HTMLPart: htmlBody,
-                Attachments: attachments
-              }
-            ]
-          })
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [messageObject]
+    });
 
   request
       .then((result) => {
